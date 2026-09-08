bf4f52e2f332662a13d200affe3dd6caeefd591b
mk0016795-maker <mk0016795@gmail.com>
2026-09-07 21:56:09 -0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,10 +14,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     defaultConfig {
         applicationId = "com.example.health_hive"
         minSdk = 21
